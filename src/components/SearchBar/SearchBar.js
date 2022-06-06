a0e00f4ddce04fc514cd5fa65d5d4fb71f513093
Deepak Goyal <dg887180@gmail.com>
2022-06-06 21:15:07 +0530

    add debounce for search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,31 +1,47 @@
-import { useNavigate } from "react-router-dom"
-import { useEffect,useRef } from "react"
-import { useFilter } from "../../Context/filter-context"
-export const SearchBar=()=>{
-  const {dispatch,state:{search}}=useFilter()
-  const navigate=useNavigate()
-  const inputRef=useRef("")
-useEffect(()=>{
-  if(search){   
-    navigate("/products")
-    inputRef.current.focus()
+import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useFilter } from "../../Context/filter-context";
+export const SearchBar = () => {
+  const {
+    dispatch,
+    state: { search },
+  } = useFilter();
+  const navigate = useNavigate();
+  const inputRef = useRef("");
+  useEffect(() => {
+    if (search) {
+      navigate("/products");
+      inputRef.current.focus();
+    }
+  }, [search]);
+
+  function debounce(func, delay) {
+    let timer;
+    return function (e) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(e);
+      }, delay);
+    };
   }
-},[search])
-    return(
-        <div
-          className="input-parent"
-        >
-          <input
-            className="ecom-input standard-input"
-            type="search"
-            value={search}
-            ref={inputRef}
-            onChange={(e) => dispatch({ type: "SEARCH", value: e.target.value })}
-            placeholder="Search for products, brands and more"
-          />
-          <span id="ecom-search" className="material-icons">
-            search
-          </span>
-        </div>
-    )
-}
+
+  const onChangeHandler = (e) => {
+    dispatch({ type: "SEARCH", value: e.target.value });
+  };
+
+  const betterFunc = debounce(onChangeHandler, 300);
+  return (
+    <div className="input-parent">
+      <input
+        className="ecom-input standard-input"
+        type="search"
+        ref={inputRef}
+        onChange={betterFunc}
+        placeholder="Search for products, brands and more"
+      />
+      <span id="ecom-search" className="material-icons">
+        search
+      </span>
+    </div>
+  );
+};
